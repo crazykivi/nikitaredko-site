544dfa8427d05717d4232ba64cdfa66efe698bf8
Nikita Redko <nik.ita-redko@yandex.ru>
2026-07-02 12:55:25 +0700

    Устранение скачков интерфейса при переходах между страницами

--- src/App.vue
+++ src/App.vue
@@ -5,12 +5,12 @@ import LoadingBar from './components/LoadingBar.vue'
 </script>
 
 <template>
-  <div class="min-h-screen bg-background text-foreground">
+  <div class="min-h-screen flex flex-col bg-background text-foreground">
     <LoadingBar />
-    <AppHeader />
-    <main class="pt-16">
+    <AppHeader /> 
+    <main class="flex-1 flex flex-col pt-16">
       <router-view v-slot="{ Component }">
-        <transition name="fade">
+        <transition name="fade" mode="out-in">
           <component :is="Component" />
         </transition>
       </router-view>
